b907f0fd1e5eea4a9c45500bff6d66298a39769a
sultanovttIOS <alisher192377@gmail.com>
2024-02-04 21:04:11 +0600

    Initial Commit

---
+++ HW4.7POP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4.7POP
+//
+//  Created by Alisher Sultanov on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
